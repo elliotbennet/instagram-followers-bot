08b929cbb9f58befd0a0982ad3b818a3d36241c4
Elliot Bennet <31925099+elliotbennet@users.noreply.github.com>
2020-02-15 21:34:03 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+username = "<username>" # Instagram username
+password = "<password>" # Instagram password
+target = "<target UID>" # Instagram target's userID for users
+unfollow = False # Unfollow users too?
+unfollow_blacklist = [] # Users not to unfollow

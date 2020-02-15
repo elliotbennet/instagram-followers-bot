9d2b226b296db6ef2a4dd921b601744d42017b43
Elliot Bennet <elliotbennet6@gmail.com>
2020-02-15 21:32:50 +0100

    Gitignore

--- config.py
+++
@@ -1,7 +0,0 @@
-username = "<username>" # Instagram username
-password = "<password>" # Instagram password
-target = "<target UID>" # Instagram target's userID for users
-unfollow = False # Unfollow users too?
-unfollow_blacklist = [] # Users not to unfollow
-
-test = True
